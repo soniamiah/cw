81bbc74a7e157a343fe631c2fd44ec86d209146e
soniamiah <45068612+soniamiah@users.noreply.github.com>
2018-12-15 22:35:48 +0000

    Add files via upload

---
+++ Group13_web/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django import forms
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default="Please Select")
+    birth_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
